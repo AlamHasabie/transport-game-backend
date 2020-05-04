9c8366de4140430594d17ab3beb0d8ef4f1b94ec
AlamHasabie <39056147+AlamHasabie@users.noreply.github.com>
2020-05-04 10:53:43 +0700

    Room (#2)
    * Finished new room creation
    
    * Added game html

--- index.js
+++ index.js
@@ -1,15 +1,67 @@
-var app = require('express')();
+const express = require('express');
+const app = express();
+const path = require('path');
+const roomstatepath = "/rooms/";
+const fs = require('fs');
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
 
+
+app.use(bodyParser.urlencoded());
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/index.html');
+
 });
 
+
+/** To join a room , simply send a post request with room-name and spectator*/
+/** The page should directly call for socket joining*/
+
+app.post('/game',(req,res)=>{
+    console.log(req.body);
+    
+    /**Check if game room exist */
+    var roomstatefile = path.join(__dirname , roomstatepath, req.body["room"] + ".json");
+    fs.access(roomstatefile,(err)=>{
+        if(err){
+            // Create new room with empty state
+            let empty_data = {
+                room : req.body["room"],
+                members : 0,
+                player : 0,
+                spectator : 0,
+                player_socket : [],
+                spectator_socket : [],
+                taken_questions : [],
+                event_pointer : 0,
+                reward_pointer : 0,
+                key_pointer : 0,
+                player_status : []
+            }
+
+            fs.writeFile(roomstatefile,JSON.stringify(empty_data),err=>{
+                if(err) return console.log(err);
+                res.statusCode = 200;
+                res.send("Good !");
+            });
+        } else {
+            // Check if room full
+            // Is full , then player role denied entry
+            // If
+        }
+
+    })
+})
+
 io.on('connection',(socket)=>{
+
+    
     console.log("An user connected");
     console.log(socket.id);
+
+
     socket.on('chat message',(msg)=>{
         console.log('message: ' + msg);
         io.emit('chat message', msg);
