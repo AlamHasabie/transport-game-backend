201076a8ff487deff753ac5e5d20e357498d19f6
Alam <alamhasabie@yahoo.com>
2020-05-15 21:12:39 +0700

    Added CORS

--- index.js
+++ index.js
@@ -31,6 +31,15 @@ global.userInfo = {};
 
 /** This is for testing purpose only */
 app.use(bodyParser.urlencoded());
+
+/** CORS */
+app.use(function(req, res, next) {
+
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  
+});
 app.get('/',(req,res)=>{
     res.render('index.ejs');
 });
